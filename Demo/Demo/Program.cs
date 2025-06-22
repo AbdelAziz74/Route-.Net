0f08d6f470350615e09c37af5f4a6895cfc49a57
Al-Quds <Al-Quds@ABDELAZIZ>
2025-06-22 19:19:39 +0300

    initial commit

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Demo
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1
+            //Console.WriteLine("Please Enter a Number");
+            //string input = Console.ReadLine();
+            //if (int.TryParse(input, out int number))
+            //{
+            //    Console.WriteLine($"You Number: {number}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please Enter a number");
+            //}
+            #endregion
+
+
+            #region Q2
+            string s = "74zoz";
+            try
+            {
+                int res = Convert.ToInt32(s);
+                Console.WriteLine(res);
+
+            }
+            catch(FormatException)
+            {
+
+            }
+
+            #endregion
+        }
+    }
+}
